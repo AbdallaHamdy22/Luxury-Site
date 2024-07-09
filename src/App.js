f712e5695133790dd74a10f727ad71cca2620c75
Elmansoory <100771018+Tiijan@users.noreply.github.com>
2024-07-09 20:14:18 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Login from './Components/Login-Register/Login';
-<<<<<<< Updated upstream
-
-const App = () => (
-  <>
-    <Header />
-      <Routes >
-        <Route path='/' element={ <Home/>} />
-        {/* <Route path='/Items' element={ <Items/>} />
-        <Route path='/ItemDetails/:id' element={ <ItemDetails/>} />
-        <Route path='/fav' element={<CartComponent />} />
-        <Route path="/fav" element={<Favorites />} /> */}
-        <Route path='/login' element={<Login />} />
-=======
 import NotFound from './Components/NotFound/NotFound';
 import CartComponent from './Components/Cart/Cart';
 import Items from './Components/Shopping/Items';
 import ItemDetails from './Components/ShoppingDetails/Items_details';
 import Sell from './Components/Sell/Sell';
-
+import FavComponent from './Components/Favorites/Favourites';
 import ShowProducts from './Components/ShowProducts/product';
 import ShowCategories from './Components/ShowCategories/Categories';
 import ShowBrands from './Components/ShowBrand/Brands';
 import ShowGenders from './Components/ShowGender/Genders';
-import FavComponent from './Components/favorites/Favourites'
+
 const App = () => {
   const [user, setUser] = useState(null);
 
@@ -56,9 +44,11 @@ const App = () => {
         <Route path='/ShowGenders' element={<ShowGenders />} />
         <Route path='/ItemDetails/:id' element={<ItemDetails />} />
         <Route path='*' element={<NotFound />} />
->>>>>>> Stashed changes
       </Routes>
-    <Footer />
-  </>
-);
+      <Footer />
+    </>
+  );
+};
+
+
 export default App;
