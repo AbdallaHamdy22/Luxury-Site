7811efe8e92c478a7585f0294cf49cf481750c60
Elmansoory <100771018+Tiijan@users.noreply.github.com>
2024-08-07 20:16:50 +0300

    .

--- src/axiosConfig/instance.js
+++ src/axiosConfig/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://royal-luxury.great-site.net/api/',
+    baseURL: 'https://royal-luxury.great-site.net/api/',
     headers: {
         'Content-Type': 'application/json',
     },
