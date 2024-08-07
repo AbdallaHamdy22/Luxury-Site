dace2721d32790973291d940a6b8dbe465b7614b
Elmansoory <100771018+Tiijan@users.noreply.github.com>
2024-08-07 20:13:39 +0300

    Update instance.js

--- src/axiosConfig/instance.js
+++ src/axiosConfig/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost/dashboard/luxury-site-last/api/',
+    baseURL: 'http://royal-luxury.great-site.net/api/',
     headers: {
         'Content-Type': 'application/json',
     },
